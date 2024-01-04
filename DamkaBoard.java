8c3bce606136810064655155c0acfb7a718d0b30
Daniellmass <daniel.mass@post.runi.ac.il>
2024-01-04 16:43:35 +0200

    adding the files

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,27 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int boardSize = Integer.parseInt(args[0]);
+		int i = 0;
+		while (boardSize > i) {
+			if ((i + 1) % 2 == 0) {
+				System.out.print(" *");
+			}
+			else {
+				System.out.print("* ");
+			}
+			int j = 1;
+			while (boardSize > j) {
+				if ((i + 1) % 2 == 0) {
+					System.out.print(" *");	
+				}
+				else {
+					System.out.print("* ");
+				}
+				j++;				
+			}
+			System.out.println();
+			i++;
+		}
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,20 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
-	}
+		String strOrig = args[0];
+		String strRevrs = "";
+		for (int i = strOrig.length() - 1; i >= 0; i--) {
+			strRevrs += strOrig.charAt(i);	 
+		}
+		System.out.println(strRevrs);
+		char midChar;
+		if (strOrig.length() % 2 == 0) {
+			midChar = strOrig.charAt(strOrig.length() / 2 - 1);
+		}
+		else {
+			midChar = strOrig.charAt(strOrig.length() / 2);
+		}
+		
+		System.out.println("The middle character is " + midChar);
+	}	
 }
